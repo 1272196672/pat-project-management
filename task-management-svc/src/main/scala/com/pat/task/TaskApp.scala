c97edc62a4594149f81ff1d6916929b8f7e776e7
Bobby.zx.lin <bobby.zx.lin@pingantradition.com.cn>
2023-02-28 11:01:33 +0800

    1.调整Actor创建方法，解决系统中重复Actor问题 待调整： 1.修改State以适配Task 2.完善Task与数据库联系

--- task-management-svc/src/main/scala/com/pat/task/TaskApp.scala
+++ task-management-svc/src/main/scala/com/pat/task/TaskApp.scala
@@ -4,9 +4,9 @@ import akka.actor.typed.ActorSystem
 import com.typesafe.config.ConfigFactory
 
 object TaskApp extends App {
-  ActorSystem[Nothing](Guardian(), "task-management-svc", ConfigFactory.load("application-node4.conf"))
+  ActorSystem[Nothing](Guardian(), "task-management-svc", ConfigFactory.load("application-node1.conf"))
 }
 
 object TaskApp2 extends App {
-  ActorSystem[Nothing](Guardian(), "task-management-svc", ConfigFactory.load("application-node5.conf"))
+  ActorSystem[Nothing](Guardian(), "task-management-svc", ConfigFactory.load("application-node2.conf"))
 }
